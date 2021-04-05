bba1914045239c3b1bb467f64191624f6e75a5b7
Gerd Aschemann <gerd@aschemann.net>
2021-04-05 13:53:39 +0200

    Refactor writers to fix SonarLint complexity issue

--- src/main/java/it/andreascarpino/ansible/inventory/util/AnsibleInventoryWriter.java
+++ src/main/java/it/andreascarpino/ansible/inventory/util/AnsibleInventoryWriter.java
@@ -19,6 +19,7 @@ package it.andreascarpino.ansible.inventory.util;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Collection;
 
 import it.andreascarpino.ansible.inventory.type.AnsibleGroup;
 import it.andreascarpino.ansible.inventory.type.AnsibleHost;
@@ -78,85 +79,113 @@ public class AnsibleInventoryWriter {
 	public static String write(AnsibleInventory inventory) {
 		final StringBuilder builder = new StringBuilder();
 
-		for (AnsibleHost host : inventory.getHosts()) {
-			builder.append(printHost(host));
-		}
+		appendHosts(builder, inventory.getHosts());
 
-		for (AnsibleGroup group : inventory.getGroups()) {
-			if (!group.getSubgroups().isEmpty()) {
-				builder.append(groupOfGroupHeader(group.getName()));
-
-				if (!group.getSubgroups().isEmpty()) {
-					for (AnsibleGroup g : group.getSubgroups()) {
-						builder.append(g.getName() + "\n");
-					}
-				} else {
-					builder.append("\n");
-				}
-			}
-
-			if (!group.getHosts().isEmpty()) {
-				builder.append(groupHeader(group.getName()));
-
-				for (AnsibleHost host : group.getHosts()) {
-					builder.append(printHost(host));
-				}
-			}
-
-			if (!group.getVariables().isEmpty()) {
-				builder.append(groupVarsHeader(group.getName()));
-
-				if (!group.getVariables().isEmpty()) {
-					for (AnsibleVariable variable : group.getVariables()) {
-						builder.append(variableBlock(variable) + "\n");
-					}
-				} else {
-					builder.append("\n");
-				}
-			}
-		}
+		appendGroups(builder, inventory);
 
 		return builder.toString();
 	}
 
-	public static void write(AnsibleInventory inventory, OutputStream stream) throws IOException {
-		for (AnsibleHost host : inventory.getHosts()) {
-			printHost(host, stream);
-		}
-
+	private static void appendGroups(StringBuilder builder, final AnsibleInventory inventory) {
 		for (AnsibleGroup group : inventory.getGroups()) {
-			if (!group.getSubgroups().isEmpty()) {
-				stream.write(groupOfGroupHeader(group.getName()).getBytes());
+			appendGroup(builder, group);
 
-				if (!group.getSubgroups().isEmpty()) {
-					for (AnsibleGroup g : group.getSubgroups()) {
-						stream.write((g.getName() + "\n").getBytes());
-					}
-				} else {
-					stream.write("\n".getBytes());
-				}
+			if (!group.getHosts().isEmpty()) {
+				builder.append(groupHeader(group.getName()));
+
+				appendHosts(builder, group.getHosts());
 			}
+
+			appendVariables(builder, group);
+		}
+	}
+
+	private static void appendVariables(StringBuilder builder, final AnsibleGroup group) {
+		if (!group.getVariables().isEmpty()) {
+			builder.append(groupVarsHeader(group.getName()));
+
+			if (!group.getVariables().isEmpty()) {
+				for (AnsibleVariable variable : group.getVariables()) {
+					builder.append(variableBlock(variable) + "\n");
+				}
+			} else {
+				builder.append("\n");
+			}
+		}
+	}
+
+	private static void appendGroup(StringBuilder builder, final AnsibleGroup group) {
+		if (!group.getSubgroups().isEmpty()) {
+			builder.append(groupOfGroupHeader(group.getName()));
+
+			if (!group.getSubgroups().isEmpty()) {
+				for (AnsibleGroup g : group.getSubgroups()) {
+					builder.append(g.getName() + "\n");
+				}
+			} else {
+				builder.append("\n");
+			}
+		}
+	}
+
+	private static void appendHosts(StringBuilder builder, final Collection<AnsibleHost> hosts) {
+		for (AnsibleHost host : hosts) {
+			builder.append(printHost(host));
+		}
+	}
+
+	public static void write(AnsibleInventory inventory, OutputStream stream) throws IOException {
+		appendHosts(stream, inventory.getHosts());
+
+		appendGroups(stream, inventory);
+	}
+
+	private static void appendGroups(OutputStream stream, final AnsibleInventory inventory) throws IOException {
+		for (AnsibleGroup group : inventory.getGroups()) {
+			appendSubgroups(stream, group);
 
 			if (!group.getHosts().isEmpty()) {
 				stream.write(groupHeader(group.getName()).getBytes());
 
-				for (AnsibleHost host : group.getHosts()) {
-					printHost(host, stream);
-				}
+				appendHosts(stream, group.getHosts());
 			}
+
+			appendVariables(stream, group);
+		}
+	}
+
+	private static void appendVariables(OutputStream stream, final AnsibleGroup group) throws IOException {
+		if (!group.getVariables().isEmpty()) {
+			stream.write("\n".getBytes());
+			stream.write(groupVarsHeader(group.getName()).getBytes());
 
 			if (!group.getVariables().isEmpty()) {
-				stream.write("\n".getBytes());
-				stream.write(groupVarsHeader(group.getName()).getBytes());
-
-				if (!group.getVariables().isEmpty()) {
-					for (AnsibleVariable variable : group.getVariables()) {
-						stream.write((variableBlock(variable) + "\n").getBytes());
-					}
-				} else {
-					stream.write("\n".getBytes());
+				for (AnsibleVariable variable : group.getVariables()) {
+					stream.write((variableBlock(variable) + "\n").getBytes());
 				}
+			} else {
+				stream.write("\n".getBytes());
 			}
+		}
+	}
+
+	private static void appendSubgroups(OutputStream stream, final AnsibleGroup group) throws IOException {
+		if (!group.getSubgroups().isEmpty()) {
+			stream.write(groupOfGroupHeader(group.getName()).getBytes());
+
+			if (!group.getSubgroups().isEmpty()) {
+				for (AnsibleGroup g : group.getSubgroups()) {
+					stream.write((g.getName() + "\n").getBytes());
+				}
+			} else {
+				stream.write("\n".getBytes());
+			}
+		}
+	}
+
+	private static void appendHosts(OutputStream stream, Collection<AnsibleHost> hosts) throws IOException {
+		for (AnsibleHost host : hosts) {
+			printHost(host, stream);
 		}
 	}
 
